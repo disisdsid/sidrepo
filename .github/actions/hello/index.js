90ce7dbb1efb41ba087ee6e112068ad73dbaaa6d
disisdsid <siddhanta.dash@gmail.com>
2024-01-31 15:21:27 +0530

    new files push

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,26 @@
+try {
+    
+    const core = require("@actions/core");
+    const github = require("@actions/github");
+
+
+    core.debug('Debug Message');
+    core.warning('Warning message');
+    core.error("Error message");
+
+
+    const name = core.getInput("who_to_greet");
+
+    console.log('Hello ${name}');
+
+    const time = new Date();
+    core.setOutput("time" , time.toTimeString());
+    
+    core.exportVariable("HELLO_TIME", time);
+
+    core.startGroup("Logging GitHub context");
+    console.log(JSON.stringify(github.context, null, 2));
+    core.endGroup();
+} catch (error) {
+    core.setFailed(error.message);
+}
